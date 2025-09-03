d5da3626c297f0b9cba5704388b9909445d545a3
Sourabh4358 <sourabhvasedar@gmail.com>
2025-09-03 09:02:13 +0530

    Initial commit

--- client/src/api.js
+++ client/src/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
-
+//Vite-api-url from render
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
   headers: { "Content-Type": "application/json" },
+  
 });
 
 export default api;
